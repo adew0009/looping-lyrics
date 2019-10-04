18bcc573d2a8d23b14085e703b351b8c889bc9a9
adew0009 <adew0009@algonquinlive.com>
2019-10-03 20:37:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const lyrics = [
     ['cow', 'moo'],
-    ['pig', 'oink'],
-    ['duck', 'quack']
+['pig', 'oink'],
+['duck', 'quack']
   ]
-  
   for (const lyric of lyrics) {
-    console.log(`Old MACDONALD had a farm
-  E-I-E-I-O
-  And on his farm he had a ${lyric[0]}
-  E-I-E-I-O
-  With a ${lyric[1]} ${lyric[1]} here
-  And a ${lyric[1]} ${lyric[1]} there
-  Here a ${lyric[1]}, there a ${lyric[1]}
-  Everywhere a ${lyric[1]} ${lyric[1]}
-  Old MacDonald had a farm
-  E-I-E-I-O
-  `)
+console.log(`Old MACDONALD had a farm
+E-I-E-I-O
+And on his farm he had a ${lyric[0]}
+E-I-E-I-O
+With a ${lyric[1]} ${lyric[1]} here
+And a ${lyric[1]} ${lyric[1]} there
+Here a ${lyric[1]}, there a ${lyric[1]}
+Everywhere a ${lyric[1]} ${lyric[1]}
+Old MacDonald had a farm
+E-I-E-I-O
+`)
   }
-  
-  const bingos = [
-    {action: '', bingo: 'B-I-N-G-O'},
-    {action: '(clap)', bingo: '-I-N-G-O'},
-    {action: '(clap)-(clap)', bingo: '-N-G-O'},
-    {action: '(clap)-(clap)-(clap)', bingo: '-G-O'},
-    {action: '(clap)-(clap)-(clap)-(clap)', bingo: '-O'},
-    {action: '(clap)-(clap)-(clap)-(clap)-(clap)', bingo: ''}
+   const bingos = [
+{action: '', bingo: 'B-I-N-G-O'},
+{action: '(clap)', bingo: '-I-N-G-O'},
+{action: '(clap)-(clap)', bingo: '-N-G-O'},
+{action: '(clap)-(clap)-(clap)', bingo: '-G-O'},
+{action: '(clap)-(clap)-(clap)-(clap)', bingo: '-O'},
+{action: '(clap)-(clap)-(clap)-(clap)-(clap)', bingo: ''}
   ]
   for (const bingo of bingos) {
     console.log(`There was a farmer who had a dog,
@@ -34,43 +32,56 @@ const lyrics = [
   ${bingo.action} ${bingo.bingo}
   And Bingo was his name-o.`)
   }
-  
-  const days = [
-    'first',
-    'second',
-    'third',
-    'sixth',
-    'seventh',
-    'eighth',
-    'ninth',
-    'tenth',
-    'eleventh',
-    'twelfth'
+  const verses = [
+    { number: 'one', action: 'suck his thumb'},
+    { number: 'two', action: 'tie his shoe'},
+    { number: 'three', action: 'climb a tree'},
+    { number: 'four', action: 'shut the door'},
+    { number: 'five', action: 'take a dive'},
   ]
   
-  const gifts = [
-    '12 Drummers of Drumming',
-    'Eleven Pipers Piping',
-    'Ten Lords a Leaping',
-    'Nine Ladies Dancing',
-    'Eight Maids a Milking',
-    'Seven Swans a Swimming',
-    'Six Geese a Laying',
-    'Five Golden Rings',
-    'Four Calling Birds',
-    'Three French Doves',
-    'Two Turtle Doves',
-    'a Partridge in a Pear Tree'
-  ]
-  console.log('12 Days of Christmas')
-  for (const day of days) {
-    console.log(`On the ${day} of Christmas my true love gave to me: `)
+  console.log('5 Little Ducks')
   
-    for (let i = 11 - days.indexOf(day); i < 12; i++) {
-      if (i === 11 && days.indexOf(day) > 0) {
-        console.log(` and ${gifts[i]}`)
-      } else {
-        console.log(gifts[i])
-      }
+{
+      console.log(`5 little ducks went out one day `)
+      console.log('Over the hills and far away')
+      console.log(`Mother duck said  “Quack, quack, quack, quack” `)
+      console.log('But only 4 little ducks came back.')
     }
+{
+  
+    console.log(`4 little ducks went out one day… `)
+    console.log(`Over the hills and far away`)
+    console.log(`Mother duck said  “Quack, quack, quack, quack” `)
+    console.log('But only 3 little ducks came back.')
   }
+  {
+
+  console.log(`3 little ducks went out one day… `)
+  console.log(`Over the hills and far away`)
+  console.log(`Mother duck said  “Quack, quack, quack, quack” `)
+  console.log('But only 2 little ducks came back.')
+}
+{
+
+    console.log(`2 little ducks went out one day…`)
+    console.log(`Over the hills and far away`)
+    console.log(`Mother duck said  “Quack, quack, quack, quack” `)
+    console.log('But only 1 little duck came back.')
+  }
+  {
+
+    console.log(`1 little duck went out one day…`)
+    console.log(`Over the hills and far away`)
+    console.log(`Mother duck said  “Quack, quack, quack, quack” `)
+    console.log('But none of the five little ducks came back')
+  }
+  {
+
+    console.log(`Sad mother duck went out one day…`)
+    console.log(`Over the hills and far away`)
+    console.log(`Mother duck said  “Quack, quack, quack, quack” `)
+    console.log('And all of the five little ducks came back.')
+  }
+
+  
